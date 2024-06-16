1a232dd9f3ff695fd1bc3e909b120b3098491069
KTop Developer <ktopdeveloper@gmail.com>
2024-06-15 22:01:40 -0400

    feat: pokemon api

---
+++ PokemonAPI/Controllers/PokemonsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PokemonAPI.Models;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace PokemonAPI.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PokemonsController : ControllerBase
+    {
+        private static List<Pokemon> Pokemons = new List<Pokemon>();
+    
+        public PokemonsController() {
+            var jsonData = System.IO.File.ReadAllText("pokemon.json");
+            Pokemons = JsonConvert.DeserializeObject<List<Pokemon>>(jsonData);
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Pokemon>> GetPokemons()
+        {
+            return Ok(Pokemons);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Pokemon> GetPokemon(int id)
+        {
+            var pokemon = Pokemons.FirstOrDefault(p => p.Number == id);
+            if (pokemon == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(pokemon);
+        }
+    }
+}
---
+++ PokemonAPI/Models/Pokemon.cs
@@ -0,0 +1,38 @@
+﻿namespace PokemonAPI.Models
+{
+    public struct PokemonState
+    {
+        public string Name { get; set; }
+        public int Value { get; set; }
+    }
+
+    public struct PokemonEvolution
+    {
+        public string? From { get; set; }
+        public string[] To { get; set; }
+    }
+
+    public class Pokemon
+    {
+        public int Number { get; set; }
+        public string Name { get; set; }
+
+        public string Generation { get; set; }
+
+        public int Height { get; set; }
+
+        public int Weight { get; set; }
+
+        public string[] Types { get; set; }
+
+        public PokemonState[] Stats  { get; set; }
+
+        public string[] Moves {  get; set; }
+
+        public string[] Abilities { get; set; }
+
+        public PokemonEvolution Evolution { get; set; }
+
+        public string Image { get; set; }
+    }
+}
